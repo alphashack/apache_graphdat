70f18c49748c518b76dc61d0077b583e3cb2233e
gareth <danger@alphashack.com>
2013-01-08 19:38:55 +0700

    only linking the rt library on linux

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 SYSTEM="linux"
+CLOCK_INCLUDE="-l rt"
 
 # check to make sure we have apxs or apxs2
 if [ -f "/etc/debian_version" ];
@@ -17,6 +18,7 @@ else
     if [[ `uname` == "Darwin" ]]; then
         SYSTEM="osx"
         BUILDWITH="apxs"
+        CLOCK_INCLUDE=""
     else
         SYSTEM="unknown"
         echo "unknown operating system: sorry we only support linux and osx right now" >&2
@@ -24,5 +26,20 @@ else
     fi
 fi
 
-sudo $BUILDWITH -c -i -a -n graphdat -I lib/module_graphdat/os -l rt -I lib/module_graphdat/os/$SYSTEM lib/module_graphdat/list.c lib/module_graphdat/os/$SYSTEM/timehelper.c lib/module_graphdat/os/$SYSTEM/mutex.c lib/module_graphdat/os/$SYSTEM/thread.c lib/module_graphdat/os/$SYSTEM/socket.c lib/module_graphdat/msgpack/src/objectc.c lib/module_graphdat/msgpack/src/unpack.c lib/module_graphdat/msgpack/src/version.c lib/module_graphdat/msgpack/src/vrefbuffer.c lib/module_graphdat/msgpack/src/zone.c lib/module_graphdat/graphdat.c mod_graphdat.c
+sudo $BUILDWITH -c -i -a -n graphdat \
+    -I lib/module_graphdat/os \
+    $CLOCK_INCLUDE \
+    -I  lib/module_graphdat/os/$SYSTEM \
+        lib/module_graphdat/list.c \
+        lib/module_graphdat/os/$SYSTEM/timehelper.c \
+        lib/module_graphdat/os/$SYSTEM/mutex.c \
+        lib/module_graphdat/os/$SYSTEM/thread.c \
+        lib/module_graphdat/os/$SYSTEM/socket.c \
+        lib/module_graphdat/msgpack/src/objectc.c \
+        lib/module_graphdat/msgpack/src/unpack.c \
+        lib/module_graphdat/msgpack/src/version.c \
+        lib/module_graphdat/msgpack/src/vrefbuffer.c \
+        lib/module_graphdat/msgpack/src/zone.c \
+        lib/module_graphdat/graphdat.c \
+        mod_graphdat.c
 
